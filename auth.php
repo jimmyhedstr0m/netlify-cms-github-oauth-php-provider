25bd8619c8f3d25161fca0ea93f2caef956414b4
Jimmy Hedström <jimmy.hedstrom93@gmail.com>
2023-02-03 21:48:20 +0100

    Update quotation marks for consistency

--- auth.php
+++ auth.php
@@ -2,8 +2,8 @@
 include "vars.php";
 session_start();
 
-$state = hash('sha512', mt_rand());
-$_SESSION['oauth2state'] = $state;
+$state = hash("sha512", mt_rand());
+$_SESSION["oauth2state"] = $state;
 
 $response_url = "https://github.com/login/oauth/authorize?response_type=code&client_id=".$client_id."&scope=repo,user&state=".$state;
 header("Location: ".$response_url, true, 302);
